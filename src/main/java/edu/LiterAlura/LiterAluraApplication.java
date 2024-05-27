0655bbcf1e15306789a893b0ba821a9d9a8bb5aa
Elisabete-MO <bete.mrt@gmail.com>
2024-05-27 16:05:31 -0300

    :sparkles: feat: PrincipalController

--- src/main/java/edu/LiterAlura/LiterAluraApplication.java
+++ src/main/java/edu/LiterAlura/LiterAluraApplication.java
@@ -1,7 +1,9 @@
 package edu.LiterAlura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LiterAluraApplication {
@@ -10,4 +12,10 @@ public class LiterAluraApplication {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(PrincipalController principalController) {
+		return args -> {
+			principalController.showMenu();
+		};
+	}
 }
---
+++ src/main/java/edu/LiterAlura/controllers/PrincipalController.java
@@ -0,0 +1,12 @@
+package edu.LiterAlura.controllers;
+
+public class PrincipalController {
+
+    public void showMenu() {
+        System.out.println("Bem vindo ao sistema de cursos da Alura");
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Cursos");
+        System.out.println("2 - Categorias");
+        System.out.println("3 - Sair");
+    }
+}
